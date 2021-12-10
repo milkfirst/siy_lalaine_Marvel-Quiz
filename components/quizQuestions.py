b9bec495f6998434a2710a1c254fed2087c7f71d
LiL <90929523+milkfirst@users.noreply.github.com>
2021-12-09 21:31:46 -0500

    Update quizQuestions.py

--- components/quizQuestions.py
+++ components/quizQuestions.py
@@ -1,8 +1,8 @@
 from components.quizTally import tally
 
-print("\U0001F911"+"WELCOME TO MY LITTLE GUESSING GAME!"+"\U0001F911")
+print("\U0001F911"+" WELCOME TO MY LITTLE GUESSING GAME! "+"\U0001F911")
 print("Pick a character: Hulk, Spiderman, Gamora or Black Widow")
-print("Let me guess who you're thinking about!"+"\U0001F92B")
+print("Let me guess who you're thinking about! "+"\U0001F92B")
 
 ans = input("is your character male or female? (y for male / n for female): ")
 tally(ans, "male")
