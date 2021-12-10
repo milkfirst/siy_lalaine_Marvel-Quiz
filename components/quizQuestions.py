1ed3d0c612b963c0e633ddecdc1f83e357a75a77
LiL <90929523+milkfirst@users.noreply.github.com>
2021-12-09 21:29:41 -0500

    emojis

--- components/quizQuestions.py
+++ components/quizQuestions.py
@@ -1,8 +1,8 @@
 from components.quizTally import tally
 
-print("WELCOME TO MY LITTLE GUESSING GAME!")
+print("U+1F911"+"WELCOME TO MY LITTLE GUESSING GAME!"+"U+1F911")
 print("Pick a character: Hulk, Spiderman, Gamora or Black Widow")
-print("Let me guess who you're thinking about")
+print("Let me guess who you're thinking about!"+"U+1F92B")
 
 ans = input("is your character male or female? (y for male / n for female): ")
 tally(ans, "male")
