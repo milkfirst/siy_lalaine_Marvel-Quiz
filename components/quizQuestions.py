7592d402b00a822da1098e8904e7d4f8396af6c9
LiL <90929523+milkfirst@users.noreply.github.com>
2021-12-09 21:14:38 -0500

    FINALLY

--- components/quizQuestions.py
+++ components/quizQuestions.py
@@ -1,13 +1,13 @@
 from components.quizTally import tally
 
-ans = input("is your character male or female? (m/f) ")
+ans = input("is your character male or female? (y for male / n for female): ")
 tally(ans, "male")
 
-ans = input("is your character from Earth? (y/n) ")
+ans = input("is your character from Earth? (y/n): ")
 tally(ans, "earth")
 
-ans = input("does your wear a mask? (y/n) ")
+ans = input("does your wear a mask? (y/n): ")
 tally(ans, "mask")
 
-ans = input("is your character human? (m/f) ")
+ans = input("is your character human? (m/f): ")
 tally(ans, "human")
