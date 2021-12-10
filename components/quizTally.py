37385988ee6cb481136b5bb3dfd6653d6d6cc72c
LiL <90929523+milkfirst@users.noreply.github.com>
2021-12-09 20:30:56 -0500

    update this

--- components/quizTally.py
+++ components/quizTally.py
@@ -1,20 +1,7 @@
 from components import vars
 
-def total(value):
-
-    if value == 0:
-        vars.character = vars.characters[3]
-        print("It's " + vars.character)
-    # add some emoji icons, or show the character image
-
-    elif value == 20:
-        vars.character = vars.characters[2]
-        print("It's " + vars.character)
-
-    elif value == 30:
-        vars.character = vars.characters[0]
-        print("It's " + vars.character)
-        
+def tally(answer, value):
+    if answer == "y":
+        ans = True
     else:
-        vars.character = vars.characters[1]
-        print("It's " + vars.character)
+        ans = False
--- components/vars.py
+++ components/vars.py
@@ -1,4 +1,11 @@
-quizTotal = 0
 
-characters = ["Hulk", "Spiderman", "Black Widow", "Gamora"]
-images = []
+database = [
+    
+    {"name":"Hulk", "male": True, "earth": True, "mask": False, "human": True},
+
+    {"name":"Spiderman", "male": True, "earth": True, "mask": True},
+
+    {"name":"Black Widow", "male": False, "earth": True, "mask": False, "human": True},
+
+    {"name":"Gamora", "male": False, "earth": False, "mask": False, "human": False}
+]
